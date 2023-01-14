9c98c4312fc04d6bde94f6d518d4bea52bf87b8f
SkyBlaise <https://github.com/SkyBlaise99>
2023-01-14 16:55:10 +0800

    Use fetch to get users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+const api = "https://tybbit-backend.onrender.com/get"
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = () => {
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }
+
+  useEffect(() => { fetchData(); }, [])
+
   return (
     <div className="App">
       <header className="App-header">
