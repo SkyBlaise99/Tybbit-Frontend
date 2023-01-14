f489f36b6256ce7930b96d13016ef15a8e3f879f
SkyBlaise <https://github.com/SkyBlaise99>
2023-01-14 17:35:58 +0800

    update display

--- src/App.js
+++ src/App.js
@@ -15,18 +15,37 @@ function App() {
   useEffect(() => { fetchData(); }, [])
 
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <h1>Leaderboard</h1>
-        <ol>
+        <div id="profile">
+          <div className="flex">
+            <div className="info">
+              <h3 className='name'>Rank</h3>
+            </div>
+            <div className="info">
+              <h3 className='name'>Name</h3>
+            </div>
+            <div className="info">
+              <h3 className='name'>Score</h3>
+            </div>
+          </div>
           {
-            users && users.length > 0 && users.map((user) => (
-              <li>
-                {user.name} | {user.score}
-              </li>
+            users && users.length > 0 && users.map((user, index) => (
+              <div className="flex" key={index}>
+                <div className="item">
+                  <span>{index + 1}</span>
+                </div>
+                <div className="item">
+                  <span>{user.name}</span>
+                </div>
+                <div className="item">
+                  <span>{user.score}</span>
+                </div>
+              </div>
             ))
           }
-        </ol>
+        </div>
       </header>
     </div>
   );
